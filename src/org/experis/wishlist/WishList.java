9f5b8266a0e60a8d920f2ff881de874efb5e6322
serena.peschiaroli <serena.peschiaroli@gmail.com>
2024-04-05 15:17:02 +0200

    Add: exception handling in wishlist Class; writeToFile method in wishlist class

--- src/org/experis/wishlist/WishList.java
+++ src/org/experis/wishlist/WishList.java
@@ -1,10 +1,15 @@
 package org.experis.wishlist;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class WishList {
+    //aggiungo il file path
+    private final static String FILE_PATH = "./resources/list.txt";
     public static void main(String[] args) {
         //apre lo scanner
         Scanner scanner = new Scanner(System.in);
@@ -16,19 +21,29 @@ public class WishList {
         //flag da inserire nel loop while
         boolean exit = false;
         //loop dove chiedere input finchè l'utente non esce
+
+
         while (!exit) {
             System.out.println("Would you like to insert a present in the list? Y/N");
             String choice = scanner.nextLine().trim().toUpperCase();
             switch (choice){
                 case "Y":
-                    System.out.println("Insert the present's name: ");
-                    name= scanner.nextLine();
-                    System.out.println("Insert the present's price: ");
-                    price = Double.parseDouble(scanner.nextLine());
-                    //aggiungere il regalo inserito nella lista
-                    presents.add(new Present(name, price));
-                    //stampare il recap della lista
-                    System.out.println("The list contains " + presents.size() + " presents");
+
+                    try{
+                        System.out.println("Insert the present's name: ");
+                        name= scanner.nextLine();
+                        System.out.println("Insert the present's price: ");
+                        price = Double.parseDouble(scanner.nextLine());
+                        //aggiungere il regalo inserito nella lista
+                        presents.add(new Present(name, price));
+                        //stampare il recap della lista
+                        System.out.println("The list contains " + presents.size() + " presents");
+                    }catch(NumberFormatException e) {
+                        System.out.println("Error: The price must be a number. Please try again.");
+                    }catch (IllegalArgumentException e){
+                        System.out.println("Error: " + e.getMessage() + " Please try again.");
+                    }
+
                     break;
                 case "N":
                     //se "N", esce dal programma
@@ -50,5 +65,21 @@ public class WishList {
             System.out.println(present);
         }
 
+    }
+
+    //metodo per scrivere sul file
+    private static boolean writeToFile(ArrayList<Present> presents){
+
+        File presentFile = new File (FILE_PATH);
+        boolean written = false;
+        try(FileWriter writer = new FileWriter(presentFile, false)){
+            for (Present present : presents){
+                writer.write(present.getName() + "," + present.getPrice() + "\n");
+            }
+            written = true;
+        }catch (IOException e){
+            System.out.println("Unable to write to file " + e.getMessage());
+        }
+        return written;
     }
 }
